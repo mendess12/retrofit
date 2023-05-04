031b9e7ee7b51d08780800af40720d0442c9c55f
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-05-04 15:02:33 +0300

    Ádd data model class

---
+++ app/src/main/java/com/example/retrofit/model/DataItem.kt
@@ -0,0 +1,14 @@
+package com.example.retrofit.model
+
+import com.google.gson.annotations.SerializedName
+
+data class DataItem(
+    @SerializedName("body")
+    val body: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("userId")
+    val userId: Int
+)
