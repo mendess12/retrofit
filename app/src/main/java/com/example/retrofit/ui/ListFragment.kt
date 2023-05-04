d69ddf1d4532d4847740885154c447906c380028
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-05-04 16:12:24 +0300

    Show data in recyclerview

--- app/src/main/java/com/example/retrofit/ui/ListFragment.kt
+++ app/src/main/java/com/example/retrofit/ui/ListFragment.kt
@@ -5,21 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.retrofit.R
+import com.example.retrofit.adapter.DataAdapter
+import com.example.retrofit.databinding.FragmentListBinding
+import com.example.retrofit.viewmodel.ListFragmentViewModel
 
 class ListFragment : Fragment() {
 
+    private lateinit var binding: FragmentListBinding
+    private val viewModel: ListFragmentViewModel by viewModels()
+    private lateinit var dataAdapter: DataAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentListBinding.bind(view)
+
+        binding.recyclerView.setHasFixedSize(true)
+        linearLayoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.layoutManager = linearLayoutManager
+        observeLiveData()
     }
 
+    private fun observeLiveData() {
+        viewModel.dataList.observe(viewLifecycleOwner) {
+            if (it != null) {
+                dataAdapter = DataAdapter(it)
+                dataAdapter.notifyDataSetChanged()
+                binding.recyclerView.adapter = dataAdapter
+            } else {
+                Toast.makeText(requireContext(), "Livedata is null", Toast.LENGTH_LONG).show()
+            }
+        }
+        viewModel.getData()
+    }
 }
--- app/src/main/java/com/example/retrofit/viewmodel/ListFragmentViewModel.kt
+++ app/src/main/java/com/example/retrofit/viewmodel/ListFragmentViewModel.kt
@@ -1,10 +1,11 @@
 package com.example.retrofit.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.retrofit.model.DataItem
 import com.example.retrofit.repository.DataRepository
 
-class ListFragmentViewModel {
+class ListFragmentViewModel : ViewModel() {
 
     private val repository = DataRepository()
     val dataList = MutableLiveData<List<DataItem>>()
